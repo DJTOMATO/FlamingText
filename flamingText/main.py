2d8440d515f288a2487473f16cf3682b6b04cdcb
Glas <48269777+DJTOMATO@users.noreply.github.com>
2024-01-06 04:10:10 -0300

    Update main.py

--- flamingText/main.py
+++ flamingText/main.py
@@ -4,23 +4,22 @@ from urllib.parse import urlencode
 
 
 class FlamingText:
-    _url = "https://example.com/flamingtext?"  # Replace with your actual URL
-    url = "https://flamingtext.com/net-fu/image_output.cgi?_comBuyRedirect=false&imageWidth=400&imageHeight=150"
+    _url = "https://flamingtext.com/net-fu/image_output.cgi?_comBuyRedirect=false&imageWidth=400&imageHeight=150"
 
     def __init__(self, **kwargs):
         self._kwargs = kwargs
 
-    async def _generateParams(self):
+    def _generateParams(self):
         if len(self._kwargs) == 0:
             raise Exception("Parameter name and script are required")
         return "&" + urlencode(dict(self._kwargs))
 
-    async def _generateURL(self):
-        params = await self._generateParams()
+    def _generateURL(self):
+        params = self._generateParams()
         return self._url + params
 
     async def process(self):
-        url = await self._generateURL()
+        url = self._generateURL()
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 return await response.text()
